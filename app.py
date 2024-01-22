713cc094f6a839c88f3226eb06e936da06064f79
salman_04 <salmankhan20808@gmail.com>
2024-01-22 15:20:59 +0530

    code push

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("Email/SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+button = st.button('Predict')
+
+if button and len(input_sms)>0:
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+
+    # 3. predict
+    result = model.predict(vector_input)[0]
+
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    elif result == 0:
+        st.header("Not Spam")
+    else:
+        st.header("something went wrong")
+
+elif button and len(input_sms) == 0:
+    st.header("please Enter the msg")
